5892e4c85e161b8316a517f4fa915c8ec34d6ae5
G-duc <gduc1511@gmail.com>
2021-07-12 13:52:25 +0700

    Display clicked button

--- 1811064697_Pham_Gia_Duc_BigSchool/ViewModels/CoursesViewModel.cs
+++ 1811064697_Pham_Gia_Duc_BigSchool/ViewModels/CoursesViewModel.cs
@@ -4,11 +4,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace _1811064697_Pham_Gia_Duc_BigSchool.ViewModels
 {
     public class CoursesViewModel
     {
         public IEnumerable<Course> UpcommingCourses { get; set; }
         public bool ShowAction { get; set; }
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesViewModel()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+ 
+        public bool CheckFollowing(string followeeId, string userId)
+        {
+            var listFollowing = _dbContext.Followings
+                .Where(c => c.FollowerId == userId)
+                .ToList();
+
+            foreach (var i in listFollowing)
+            {
+                if (i.FolloweeId.Equals(followeeId))
+                {
+                    return true;
+                }    
+            }
+            return false;
+        } 
+
+        public bool CheckAttending(int courseId, string userId)
+        {
+            var listAttending = _dbContext.Attendances
+                .Where(c => c.AttendeeId == userId)
+                .ToList();
+            foreach(var i in listAttending)
+            {
+                if(i.CourseId == courseId)
+                {
+                    return true;
+                }    
+            }
+            return false;
+        }
     }
 }
