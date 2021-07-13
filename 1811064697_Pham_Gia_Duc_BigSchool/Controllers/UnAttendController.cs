b41827c3054835a1d282bf3ac1305c10b307ab31
G-duc <gduc1511@gmail.com>
2021-07-13 22:13:21 +0700

    Finish 1

---
+++ 1811064697_Pham_Gia_Duc_BigSchool/Controllers/UnAttendController.cs
@@ -0,0 +1,42 @@
+﻿using _1811064697_Pham_Gia_Duc_BigSchool.DTOs;
+using _1811064697_Pham_Gia_Duc_BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _1811064697_Pham_Gia_Duc_BigSchool.Controllers
+{
+    public class UnAttendController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public UnAttendController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IHttpActionResult UnAttend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var DelAttend = _dbContext.Attendances
+                .FirstOrDefault(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId);
+
+            if(DelAttend == null)
+            {
+                return BadRequest("The Attendance does not exsists !");
+            }
+            _dbContext.Attendances.Remove(DelAttend);
+            _dbContext.SaveChanges();
+
+            return Ok();
+
+        }
+
+    }
+}
---
+++ 1811064697_Pham_Gia_Duc_BigSchool/Controllers/UnFollowController.cs
@@ -0,0 +1,43 @@
+﻿using _1811064697_Pham_Gia_Duc_BigSchool.DTOs;
+using _1811064697_Pham_Gia_Duc_BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _1811064697_Pham_Gia_Duc_BigSchool.Controllers
+{
+    public class UnFollowController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public UnFollowController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IHttpActionResult UnFollow(FollowingDto followDto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var DelFollowing = _dbContext
+                .Followings
+                .FirstOrDefault(a => a.FollowerId == userId && a.FolloweeId == followDto.FolloweeId);
+
+            if (DelFollowing == null)
+            {
+                return BadRequest("The Follow  is not exists!");
+            }
+            _dbContext.Followings.Remove(DelFollowing);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
+
